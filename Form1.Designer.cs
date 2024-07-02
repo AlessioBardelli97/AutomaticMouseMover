c0cf3c7b26b04a199efe200c0ec334053ed058f3
Alessio Bardelli <alessio.bardelli@intecs.it>
2024-07-02 18:16:25 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AutomaticMouseMover
+{
+  partial class AutomaticMaouseMoverForm
+  {
+    /// <summary>
+    /// Variabile di progettazione necessaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Pulire le risorse in uso.
+    /// </summary>
+    /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Codice generato da Progettazione Windows Form
+
+    /// <summary>
+    /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+    /// il contenuto del metodo con l'editor di codice.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutomaticMaouseMoverForm));
+      this.StartMouseMoveBtn = new System.Windows.Forms.Button();
+      this.StopMouseMoveBtn = new System.Windows.Forms.Button();
+      this.PanelButtons = new System.Windows.Forms.FlowLayoutPanel();
+      this.CloseBtn = new System.Windows.Forms.Button();
+      this.PanelButtons.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // StartMouseMoveBtn
+      // 
+      this.StartMouseMoveBtn.Location = new System.Drawing.Point(3, 3);
+      this.StartMouseMoveBtn.Name = "StartMouseMoveBtn";
+      this.StartMouseMoveBtn.Size = new System.Drawing.Size(110, 23);
+      this.StartMouseMoveBtn.TabIndex = 0;
+      this.StartMouseMoveBtn.Text = "Muovi Mouse";
+      this.StartMouseMoveBtn.UseVisualStyleBackColor = true;
+      this.StartMouseMoveBtn.Click += new System.EventHandler(this.StartMouseMoveBtnClick);
+      // 
+      // StopMouseMoveBtn
+      // 
+      this.StopMouseMoveBtn.Enabled = false;
+      this.StopMouseMoveBtn.Location = new System.Drawing.Point(3, 32);
+      this.StopMouseMoveBtn.Name = "StopMouseMoveBtn";
+      this.StopMouseMoveBtn.Size = new System.Drawing.Size(110, 23);
+      this.StopMouseMoveBtn.TabIndex = 3;
+      this.StopMouseMoveBtn.Text = "Stop";
+      this.StopMouseMoveBtn.UseVisualStyleBackColor = true;
+      this.StopMouseMoveBtn.Click += new System.EventHandler(this.StopMouseMoveBtnClick);
+      // 
+      // PanelButtons
+      // 
+      this.PanelButtons.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.PanelButtons.AutoSize = true;
+      this.PanelButtons.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.PanelButtons.Controls.Add(this.StartMouseMoveBtn);
+      this.PanelButtons.Controls.Add(this.StopMouseMoveBtn);
+      this.PanelButtons.Controls.Add(this.CloseBtn);
+      this.PanelButtons.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+      this.PanelButtons.Location = new System.Drawing.Point(14, 14);
+      this.PanelButtons.Margin = new System.Windows.Forms.Padding(5);
+      this.PanelButtons.Name = "PanelButtons";
+      this.PanelButtons.Size = new System.Drawing.Size(116, 87);
+      this.PanelButtons.TabIndex = 4;
+      // 
+      // CloseBtn
+      // 
+      this.CloseBtn.Location = new System.Drawing.Point(3, 61);
+      this.CloseBtn.Name = "CloseBtn";
+      this.CloseBtn.Size = new System.Drawing.Size(110, 23);
+      this.CloseBtn.TabIndex = 4;
+      this.CloseBtn.Text = "Chiudi";
+      this.CloseBtn.UseVisualStyleBackColor = true;
+      this.CloseBtn.Click += new System.EventHandler(this.CloseBtnClick);
+      // 
+      // AutomaticMaouseMoverForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.ClientSize = new System.Drawing.Size(152, 117);
+      this.ControlBox = false;
+      this.Controls.Add(this.PanelButtons);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Location = new System.Drawing.Point(300, 300);
+      this.Name = "AutomaticMaouseMoverForm";
+      this.Text = "Automatic Mouse Mover";
+      this.PanelButtons.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button StartMouseMoveBtn;
+    private System.Windows.Forms.Button StopMouseMoveBtn;
+    private System.Windows.Forms.FlowLayoutPanel PanelButtons;
+    private System.Windows.Forms.Button CloseBtn;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutomaticMouseMover
+{
+  public partial class AutomaticMaouseMoverForm : Form
+  {
+    private bool mouseMove;
+    private readonly Random Random;
+    private Thread AnimationThread;
+
+    private static readonly int AnimationDurationMilliseconds = 1500;
+    private static readonly int AnimationSleepMilliseconds = 25;
+    private static readonly int AnimationDelayMilliseconds = 250;
+
+    public AutomaticMaouseMoverForm()
+    {
+      InitializeComponent();
+      Random = new Random((int) GetTicksInMilliseconds());
+      mouseMove = false;
+      AnimationThread = null;
+    }
+
+    private void StartMouseMoveBtnClick(object sender, EventArgs e)
+    {
+      mouseMove = true;
+      StopMouseMoveBtn.Enabled = true;
+      StartMouseMoveBtn.Enabled = false;
+      AnimationThread = new Thread(new ThreadStart(() =>
+      {
+        while (mouseMove)
+        {
+          var OldX = Cursor.Position.X;
+          var OldY = Cursor.Position.Y;
+
+          var TotalDx = Random.Next(0, Screen.PrimaryScreen.Bounds.Width) - OldX;
+          var TotalDy = Random.Next(0, Screen.PrimaryScreen.Bounds.Height) - OldY;
+
+          var startAnimation = GetTicksInMilliseconds();
+
+          while (mouseMove && (GetTicksInMilliseconds() - startAnimation < AnimationDurationMilliseconds))
+          {
+            Thread.Sleep(AnimationSleepMilliseconds);
+
+            if (!mouseMove)
+              break;
+
+            var currentTicks = GetTicksInMilliseconds();
+
+            int dx = (int)(TotalDx * (currentTicks - startAnimation) / AnimationDurationMilliseconds);
+            int dy = (int)(TotalDy * (currentTicks - startAnimation) / AnimationDurationMilliseconds);
+
+            Cursor.Position = new Point(OldX + dx, OldY + dy);
+          }
+
+          Thread.Sleep(AnimationDelayMilliseconds);
+        }
+
+      }));
+      AnimationThread.Start();
+    }
+
+    private void StopMouseMoveBtnClick(object sender, EventArgs e)
+    {
+      mouseMove = false;
+      AnimationThread?.Join();
+      StartMouseMoveBtn.Enabled = true;
+      StopMouseMoveBtn.Enabled = false;
+    }
+
+    private void CloseBtnClick(object sender, EventArgs e)
+    {
+      mouseMove = false;
+      AnimationThread?.Join();
+      Application.Exit();
+    }
+    private static double GetTicksInMilliseconds()
+    {
+      return DateTime.Now.Ticks / 10_000.0;
+    }
+  }
+}
